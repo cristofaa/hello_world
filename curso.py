4069f190c5c320bee829c0bd36d64476726a7b3e
cristofaa <47372202+cristofaa@users.noreply.github.com>
2019-02-12 05:52:17 -0600

    Update curso.py

--- curso.py
+++ curso.py
@@ -12,11 +12,30 @@ ventana.geometry("600x500")
 
 def generar_carpeta_archivo_vista_y_controlador():
   os.system("mkdir nuevo_proyecto;echo #archivo_vista > vista.py;echo #archivo_controlador > controlador.py")
+  archivo_vista = open("nuevo_proyecto/vista.py","U")
+  archivo_controlador = open("nuevo_proyecto/controlador.py","U")
+  archivo_vista.write(" from tkinter import* \nimport time,mathventana = tk(  ventana.title("titulo del archivo ") ventana.geometry("500x600") ")
+  
+  
+  
+  
+  
+  
+  
+  
+  archivo_vista.close()
+  archivo_controlador.close()
 
 
 
 
 
 
+
+
+
+
+#vista 
+btn_generar_carpertas = Button(text="generar carpetas",command = generar_carpeta_archivo_vista_y_controlador )
 
 ventana.mainloop()
