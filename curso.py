7b09e1f5a101cac6d38964e604bb358faee69dd2
cristofaa <47372202+cristofaa@users.noreply.github.com>
2019-02-12 00:12:22 -0600

    Update curso.py

--- curso.py
+++ curso.py
@@ -1,2 +1,22 @@
-import sys,os,time
-print("hola mundo ")
+from tkinter import *
+import os,sys,time
+
+ventana=Tk()
+ventana.title("ventana para idle")
+ventana.geometry("600x500")
+
+
+
+#este codigo se va a dividir en dos partes el la vista  y el controlador 
+#controlador 
+
+def generar_carpeta_archivo_vista_y_controlador():
+  os.system("mkdir nuevo_proyecto;echo #archivo_vista > vista.py;echo #archivo_controlador > controlador.py")
+
+
+
+
+
+
+
+ventana.mainloop()
